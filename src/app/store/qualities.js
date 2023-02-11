e326af75f03f067caa07e12a113037ce1a8aca89
Andrey Loginov <operator34@yandex.ru>
2023-02-11 16:40:15 +0300

    20.Внедрение redux в проекn.3. Создаем Qualities Slice.

---
+++ src/app/store/qualities.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const qualitiesSlice = createSlice({
+    name: "qualities",
+    initialState: { entities: null, isLoading: true }
+});
+
+const { reducer: qualitiesReducer } = qualitiesSlice;
+
+export default qualitiesReducer;

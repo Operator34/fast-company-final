a9117fa70daae473e75de2596f1199781d6b1d47
Andrey Loginov <operator34@yandex.ru>
2023-02-11 17:12:27 +0300

    20.Внедрение redux в проекn.4. Создаем Qualities Reducer

--- src/app/store/qualities.js
+++ src/app/store/qualities.js
@@ -1,10 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
+import qualityService from "../services/quality.service";
 
 const qualitiesSlice = createSlice({
     name: "qualities",
-    initialState: { entities: null, isLoading: true }
+    initialState: { entities: null, isLoading: true, error: null },
+
+    reducers: {
+        qualitiesRequested: (state) => {
+            state.isLoading = true;
+        },
+        qualitiesReceved: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        qualitiesRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        }
+    }
 });
 
-const { reducer: qualitiesReducer } = qualitiesSlice;
+const { reducer: qualitiesReducer, actions } = qualitiesSlice;
+const { qualitiesReceved, qualitiesRequestFailed, qualitiesRequested } =
+    actions;
+
+export const loadQualitiesList = () => async (dispatch) => {
+    dispatch(qualitiesRequested());
+    try {
+        const { content } = await qualityService.fetchAll();
+        dispatch(qualitiesReceved(content));
+    } catch (error) {
+        dispatch(qualitiesRequestFailed(error.message));
+    }
+};
 
 export default qualitiesReducer;
